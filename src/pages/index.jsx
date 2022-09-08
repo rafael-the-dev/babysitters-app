b4058dbc2bcfb7f0a169318415b7326c0ef8e084
Rafael Tivane <luislangabusiness@gmail.com>
2022-09-08 11:03:01 +0200

    create hero section and add background video

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,4 +1,8 @@
 
+import classNames from "classnames";
+
+import classes from "./styles.module.css"
+
 import Babysitters from "src/components/home-components/descubra-mais copy";
 import ComoFunciona from "src/components/como-funciona";
 import DescubraMais from "src/components/home-components/descubra-mais";
@@ -8,6 +12,15 @@ const Home = () => {
 
     return (
         <main>
+            <section>
+                <div className={classNames(classes.videoWrapper, `w-full`)}>
+                    <video autoPlay className={classNames(classes.video, `h-full object-cover w-full`)} loop muteds>
+                        <source src="https://cdn.babysits.com/content/global/hero/european/babysits_single_scene.mp4" type="video/mp4" />
+                        <source src="https://cdn.babysits.com/content/global/hero/european/babysits_single_scene.webm" type="video/webm" />
+                        Your browser does not support the video tag.
+                    </video>
+                </div>
+            </section>
             <Babysitters />
             <Seguranca />
             <ComoFunciona />

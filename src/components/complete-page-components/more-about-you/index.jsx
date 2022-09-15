1c0412e679550bb1e2ce30249c45a6a61adf6620
Rafael Tivane <luislangabusiness@gmail.com>
2022-09-15 14:43:28 +0200

    add selected chips

--- src/components/complete-page-components/more-about-you/index.jsx
+++ src/components/complete-page-components/more-about-you/index.jsx
@@ -1,6 +1,6 @@
 import { useCallback, useId, useMemo, useState } from "react"
 import classNames from "classnames";
-import { MenuItem, TextField } from "@mui/material"
+import { Chip, MenuItem, TextField } from "@mui/material"
 
 
 import Input from "src/components/default-input";
@@ -8,6 +8,7 @@ import Legend from "../legend-container"
 
 const Container = () => {
     const [ educacao, setEducacao ] = useState([]);
+    const [ linguas, setLinguas ] = useState([]);
     const id = useId();
 
     const legendMemo = useMemo(() => (
@@ -25,6 +26,31 @@ const Container = () => {
     const educacaoItemlickHandler = useCallback(prop => () => {
         setEducacao(currentList => [ ...new Set([ ...currentList, prop ])])
     }, [])
+
+    const linguasItemlickHandler = useCallback(prop => () => {
+        setLinguas(currentList => [ ...new Set([ ...currentList, prop ])])
+    }, []);
+
+    const deleteItem = useCallback((prop, func) => () => {
+        func(currentList => currentList.filter(item => item !== prop))
+    }, [])
+
+    const educacaoChipsList = useMemo(() => {
+        return (
+            <div className="flex flex-wrap">
+                {
+                    educacao.map((item, index) => (
+                        <Chip 
+                            className="bg-cyan-400 mr-3 mb-3"
+                            key={`${id}${index}`} 
+                            label={item}
+                            onDelete={deleteItem(item, setEducacao)} 
+                        />
+                    ))
+                }
+            </div>
+        );
+    }, [ deleteItem, educacao, id ]);
 
     const educacaoInputMemo = useMemo(() => {
         const list = [ "9 ano", "12º ano", "Licenciatura", "Mestrado", "Pós-graduação" ];
@@ -53,11 +79,53 @@ const Container = () => {
 
     const linguasLabelMemo = useMemo(() => (
         <label
-            className="font-semibold"
-            htmlFor="birth-date">
+            className="font-semibold mb-3"
+            htmlFor="linguas-input">
             Línguas que falo
         </label>
-    ), [])
+    ), []);
+    
+    const linguasChipsList = useMemo(() => {
+        return (
+            <div className="flex flex-wrap">
+                {
+                    linguas.map((item, index) => (
+                        <Chip 
+                            className="bg-cyan-400 mr-3 mb-3"
+                            key={`${id}${index}`} 
+                            label={item}
+                            onDelete={deleteItem(item, setLinguas)} 
+                        />
+                    ))
+                }
+            </div>
+        );
+    }, [ deleteItem, id, linguas ]);
+
+    const linguasInputMemo = useMemo(() => {
+        const list = [ "Português", "Inglês" ];
+
+        return (
+            <Input 
+                fullWidth
+                id="linguas-input"
+                select
+            >
+                {
+                    list.filter(item => !linguas.includes(item))
+                        .map((item, index) => (
+                            <MenuItem 
+                                key={`${id}${index}`} 
+                                onClick={linguasItemlickHandler(item)}
+                                value={item}>
+                                { item }
+                            </MenuItem>
+                        )
+                    )
+                }
+            </Input>
+        )
+    }, [ id, linguas, linguasItemlickHandler ]);
 
 
     const handleDateChange = useCallback((newValue) => {
@@ -71,11 +139,13 @@ const Container = () => {
                     { legendMemo }
                     <div className="flex flex-col mt-8">
                         { educacaoLabelMemo }
+                        { educacaoChipsList }
                         { educacaoInputMemo }
                     </div>
                     <div className="flex flex-col mt-8">
                         { linguasLabelMemo }
-                        
+                        { linguasChipsList }
+                        { linguasInputMemo }
                     </div>
                 </fieldset>
             </form>
